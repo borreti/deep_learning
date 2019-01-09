e14f6f3e2d4275d6637d7ddf066ed3d5066dc232
borreti <36523422+borreti@users.noreply.github.com>
2019-01-09 19:14:27 -0200

    Update and rename gato_cachorro.py to cat_dog.py

--- cat_dog.py
+++ cat_dog.py
@@ -14,55 +14,57 @@ from keras.preprocessing import image
 
 #layers sequence: convolution, pooling e flattening
 
-classificador = Sequential()
+classificator = Sequential()
 #conv2D(nFiltros, (tuple_kernel_size), (width, height, n_filters), activation function)
-classificador.add(Conv2D(32, (3,3), input_shape =(64,64,3), activation='relu'))
-classificador.add(BatchNormalization()) #batch normalization to speed up the learning
-classificador.add(MaxPooling2D(pool_size=(2,2)))
+classificator.add(Conv2D(32, (3,3), input_shape =(64,64,3), activation='relu'))
+classificator.add(BatchNormalization()) #batch normalization to speed up the learning
+classificator.add(MaxPooling2D(pool_size=(2,2)))
 
 #adding more layers
-classificador.add(Conv2D(32, (3,3), activation='relu'))
-classificador.add(BatchNormalization())
-classificador.add(MaxPooling2D(pool_size=(2,2)))
+classificator.add(Conv2D(32, (3,3), activation='relu'))
+classificator.add(BatchNormalization())
+classificator.add(MaxPooling2D(pool_size=(2,2)))
 
 #adding flatten layer
-classificador.add(Flatten())
+classificator.add(Flatten())
 
-classificador.add(Dense(units = 128, activation = 'relu'))
-classificador.add(Dropout(0.2))
-classificador.add(Dense(units = 1, activation = 'sigmoid'))
+classificator.add(Dense(units = 128, activation = 'relu'))
+classificator.add(Dropout(0.2))
+classificator.add(Dense(units = 1, activation = 'sigmoid'))
 
-classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classificator.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 #augmetattion
-gerador_treinamento = ImageDataGenerator(rescale = 1./255,
+g_training = ImageDataGenerator(rescale = 1./255,
                                          rotation_range = 7,
                                          horizontal_flip = True,
                                          shear_range = 0.2,
                                          height_shift_range = 0.07,
                                          zoom_range = 0.2)
 
-gerador_teste = ImageDataGenerator(rescale = 1./255)
+g_test = ImageDataGenerator(rescale = 1./255)
 
-base_treinamento = gerador_treinamento.flow_from_directory('dataset/training_set',
+base_training = gerador_treinamento.flow_from_directory('dataset/training_set',
                                                            target_size = (64, 64),
                                                            batch_size = 32,
                                                            class_mode = 'binary')
 
-base_teste = gerador_teste.flow_from_directory('dataset/test_set',
+base_test = gerador_teste.flow_from_directory('dataset/test_set',
                                                target_size = (64, 64),
                                                batch_size = 32,
                                                class_mode = 'binary')
 
-classificador.fit_generator(base_treinamento, steps_per_epoch = 4000 / 32,
-                            epochs = 10, validation_data = base_teste,
+classificator.fit_generator(base_training, steps_per_epoch = 4000 / 32,
+                            epochs = 10, validation_data = base_test,
                             validation_steps = 1000 / 32)
 
 
-#classificando novas imagens
+#classify new image
 
 imagem_teste = image.load_img('dataset/test_set/gato/cat.3500.jpg')
 
 imagem_teste = image.img_to_array(imagem_teste)
 imagem_teste /=255
 imagem_teste = np.expand_dims(imagem_teste, axis = 0)
+
+classificator.predict(imagem_teste)
